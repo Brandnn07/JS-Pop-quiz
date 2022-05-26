0db7edac2e56267783de9f64359fc3341f071505
brandnn07 <brandcarter4@gmail.com>
2022-05-26 13:20:31 -0400

    done again

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var array = ["Osiel", "Jordan", "Jodi", "Narivi"];
 
 function popQuiz(arr) {
-    arr.reverse()
-    return arr;
+    let input = array;
+let output = [];
+
+for(let i = 1; i <= input.length; i++) {
+
+    // determine correct array index in respect to the current iteration
+    let index = input.length - i;
+
+    // push to new array
+    output.push(input[index]);
 }
-console.log(popQuiz(array))
+
+// verify result
+console.log(output);
+}
